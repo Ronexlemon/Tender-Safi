601e33c6afeac53b828281c97706c9f6d1f5972b
“dennohkim” <kimathidennis38@gmail.com>
2022-11-28 23:54:29 +0300

    file and folder structure

---
+++ TenderSafiClient/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Logo } from '../assets/images/LogoTenderSafi.svg'
+
+const Navbar = () => {
+  return (
+    <div>
+        <img src={Logo} alt="" />
+    </div>
+  )
+}
+
+export default Navbar;
+
---
+++ TenderSafiClient/src/pages/LandingPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+const LandingPage = () => {
+  return (
+    <div>
+        <header>
+            {/* Content goes here */}
+        </header>
+        <section>
+            {/* App Features */}
+        </section>
+        <section>
+            {/* Explore */}
+        </section>
+        <section>
+            {/* World Users */}
+        </section>
+        <section>
+            {/* Email */}
+        </section>
+        <footer>
+            {/* Footer */}
+        </footer>
+    </div>
+  )
+}
+
+export default LandingPage
