b5c3af38f9bd5d17be8795320bd7d528992d739a
RonexLemon <ronexondimu@gmail.com>
2022-11-30 16:07:19 +0300

    modify the hardhat config file

--- TenderSafiBackend/hardhat.config.js
+++ TenderSafiBackend/hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({path: ".env"});
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks:{
+    goerli:{
+      url: ALCHEMY_KEY,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
